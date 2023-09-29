4790af90c8413ea95b06183002838b0ccf5f15e7
Juan Navarlaz Urtubia <jnavarlaz002@ikasle.ehu.eus>
2023-09-29 09:39:16 +0200

    cambios prueba

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -4,7 +4,7 @@ public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
     public static final int TYPE_CC = 2;
-
+    //jaja comentario
     public abstract String getLabel();
     public abstract void applyAction();
     public abstract int getCardType();
